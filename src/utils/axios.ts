002b074933fdc3816b20185014039f088bec9c27
kaleabSolomon <kaleabslmn@gmail.com>
2025-03-09 22:49:01 +0300

    update API URL in axios instance and change token key in localStorage retrieval

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Create an axios instance
-const API_URL = "http://localhost:3333/api/";
+const API_URL = "https://exuberant-jennine-sewlesew-cc283cb8.koyeb.app/";
 
 export const api = axios.create({
   baseURL: API_URL,
@@ -10,7 +10,8 @@ export const api = axios.create({
   },
 });
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
+  const token = localStorage.getItem("token");
+  console.log(token);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
