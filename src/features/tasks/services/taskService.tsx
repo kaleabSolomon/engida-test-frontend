5f2bbf62ba95b0a4e99f91d382558af5b736dc8e
kaleabSolomon <kaleabslmn@gmail.com>
2025-03-09 16:00:15 +0300

    refactor taskService to use centralized api instance for HTTP requests

--- src/features/tasks/services/taskService.tsx
+++ src/features/tasks/services/taskService.tsx
@@ -1,21 +1,20 @@
-// src/features/tasks/taskService.ts
-import axios from "axios";
 import { Task } from "../../../types/task";
+import { api } from "../../../utils/axios";
 
 const API_URL = "http://localhost:3333/api/task";
 
 export const taskService = {
   fetchTasks: async (): Promise<Task[]> => {
-    const response = await axios.get(API_URL);
+    const response = await api.get(API_URL);
     return response.data.data;
   },
 
   createTask: async (task: Task): Promise<Task> => {
-    const response = await axios.post(API_URL, task);
+    const response = await api.post(API_URL, task);
     return response.data.data;
   },
 
   deleteTask: async (taskId: string): Promise<void> => {
-    await axios.delete(`${API_URL}/${taskId}`);
+    await api.delete(`${API_URL}/${taskId}`);
   },
 };
